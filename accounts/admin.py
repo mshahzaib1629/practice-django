1e8da6606f9b4d67730b13b7dc43d4c0dbfaef75
Shahzaib <mshahzaib1629@gmail.com>
2024-07-07 18:10:39 +0500

    models added and registered in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Do display and interact with the Models from django Admin Panel
+from .models import *
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    # adding __str__ function to display customer's name instead of object (1)
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    #   CATEGORY is not the property of this model. But a tuple containing drop down values for the category property
+    CATEGORY = (
+            ('Indoor', 'Indoor'),
+            ('Out Door', 'Out Door'),
+            ) 
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+      return self.name
+
+
+class Order(models.Model):
+	STATUS = (
+			('Pending', 'Pending'),
+			('Out for delivery', 'Out for delivery'),
+			('Delivered', 'Delivered'),
+			)
+
+	#customer = 
+	#product = 
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	status = models.CharField(max_length=200, null=True, choices=STATUS)
