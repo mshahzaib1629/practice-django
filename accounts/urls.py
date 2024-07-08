6719b337767cf750bfcd2cc4bd3443e84f53d386
Shahzaib <mshahzaib1629@gmail.com>
2024-07-08 08:35:53 +0500

    in-app routing added

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
-    path("products/", views.products),
+    # we can add name to the routes, which can be used for in-app routing
+    path('', views.home, name="home"),
+    path('products/', views.products, name='products'),
     # adding url param 'pk_test'
-    path('customer/<str:pk_test>/', views.customer),
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
 ]
