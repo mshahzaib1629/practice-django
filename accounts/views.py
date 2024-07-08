bc094fc14e64daffe8c02b54024f1ddd94642a51
Shahzaib <mshahzaib1629@gmail.com>
2024-07-08 09:36:24 +0500

    added inline formsets

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# Create your views here.
+
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
 
+# Create your views here.
 def home(request):
 	orders = Order.objects.all()
 	customers = Customer.objects.all()
@@ -35,20 +37,20 @@ def customer(request, pk_test):
 	return render(request, 'accounts/customer.html',context)
 
 
-def createOrder(request):
-	# This view is responsible for rendering Order Form template and saving the Order as well
-	form = OrderForm()
+def createOrder(request, pk):
+	OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10 )
+	customer = Customer.objects.get(id=pk)
+	formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
+	#form = OrderForm(initial={'customer':customer})
 	if request.method == 'POST':
 		#print('Printing POST:', request.POST)
-		form = OrderForm(request.POST)
-		if form.is_valid():
-			# form.save() is saving the model values into DB
-			form.save()
-
-			# to redirect to a URL
+		#form = OrderForm(request.POST)
+		formset = OrderFormSet(request.POST, instance=customer)
+		if formset.is_valid():
+			formset.save()
 			return redirect('/')
 
-	context = {'form':form}
+	context = {'form':formset}
 	return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
